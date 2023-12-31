fc87bc37452aeeb83dd908c13bc2d49dfda16818
Lucas-Henrique <l.correia@escolar.ifrn.edu.br>
2023-12-31 00:56:57 -0300

    tentando arrumar o js da seleção de generos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,26 @@
-function mostrarSenha(){
+function mostrarSenha() {
     var inputPass = document.getElementById('senha')
     var btnShowPass = document.getElementById('btn-senha')
-    if(inputPass.type === 'password'){
+    if (inputPass.type === 'password') {
         inputPass.setAttribute('type', 'text')
         btnShowPass.classList.replace('bi-eye-slash', 'bi-eye')
     }
-    else{
+    else {
         inputPass.setAttribute('type', 'password')
-        btnShowPass.classList.replace('bi-eye', 'bi-eye-slash' )
+        btnShowPass.classList.replace('bi-eye', 'bi-eye-slash')
+    }
+}
+
+
+
+function mostrarSelecao() {
+    var dropDown = document.getElementById('drop')
+
+    dropDown.classList.replace('d-none', 'd-on')
+    dropDown.setAttribute('type', 'text')
+
+    if (dropDown.type === 'text') {
+        dropDown.classList.replace('d-on', 'd-none')
+        dropDown.setAttribute('type', 'off')
     }
 }
